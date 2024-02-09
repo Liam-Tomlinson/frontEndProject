e1baf1769269d75ffc19581de2d6e49015114dad
Liam Tomlinson <liamtomlinson@Liams-MacBook-Air.local>
2024-02-09 15:04:18 +0000

    delete comment button added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import './App.css'
 import AllArticles from './componants/AllArticles'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ViewSingleArticle from './componants/ViewSingleArticle';
+import ViewUsers from './componants/ViewUsers';
 
 
 function App() {
-  const [articleID, setarticleID] = useState(0)
 
   return (<>
   <Routes >
    <Route path='article/:article_ID' element={<ViewSingleArticle/>}></Route>
    <Route path='/' element={<AllArticles/>}></Route>
+   <Route path='/login' element={<ViewUsers/>}></Route>
   </Routes>
   </>
-  
    )
 }
 
---
+++ src/componants/ViewUsers.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import ViewUsersList from './insideViewUsers/ViewUsersList';
+
+const ViewUsers = () => 
+{
+    return < >
+        <h1>Who are you?</h1>
+        <ViewUsersList/>
+    </>
+    
+
+}
+
+export default ViewUsers;
